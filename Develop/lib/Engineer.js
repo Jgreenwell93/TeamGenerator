07b77074acd5e262b8ef476b48562baa1dd52a9c
Joseph Greenwell <joeygreenwell@ymail.com>
2021-04-21 18:35:32 -0500

    adding in subclasses

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("../lib/Employee");
+
+
+class Engineer extends Employee {
+    constructor(Github){
+        super (name, id, email);
+        this.github=Github;
+    }
+
+    getGithub(){
+        return this.github;
+    }
+
+    getRole(){
+        return "Engineer";
+    }
+}
+
+module.exports = Engineer;
