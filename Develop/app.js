c419ad8357880a2616d52d42512f475b402d8caf
Joseph Greenwell <joeygreenwell@ymail.com>
2021-04-22 00:14:22 -0500

    adding in pseudo coding

--- Develop/app.js
+++ Develop/app.js
@@ -4,32 +4,34 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require("./lib/htmlRenderer");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+//Global variables
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+//Inquirer Questions 
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+//Questions that fire off at initialization and after employee object is instantiated
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+//Role question (What subclass of Employee to create)
+
+//Intern Questions
+
+//Engineer Questions
+
+//Manager Questions
+
+//Prompts user if they wish to add an employee. 
+//If no, page is rendered.
+//If yes, calls employeeRole
+
+//Asks user what type of employee they wish to add 
+//(Manager, Engineer, Intern)
+
+//functions (addManager, addEngineer, addIntern) prompt role specific questions ,
+//push the object to the employees array and callback the addNewEmployee function again. 
